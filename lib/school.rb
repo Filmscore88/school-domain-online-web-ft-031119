110650a1bb664fe25cb36b1f45225c854ceb890a
Alberto Morales <cmtmorales24@gmail.com>
2019-03-20 14:39:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,11 +5,8 @@ class School
   def roster
     @roster={}
   end 
-  def add_student(name,grade)
+  def add_student=(name,grade)
     @name=name
     @grade=grade 
-    @roster[@grade]=[]
-    @roster[@grade]<< @name 
-  end 
-  
+    
 end
