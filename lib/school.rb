79294e013f5ec0ffe6e47083c74bf62c41efed0a
Alberto Morales <cmtmorales24@gmail.com>
2019-03-20 14:18:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School 
+  def initialize(name)
+    @name=name 
+  end 
+  def roster
+    @roster={}
+  end 
+  def add_student=(name,grade)
+    @name=name
+    @grade=grade 
+  end 
+end
